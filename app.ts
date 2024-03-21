70b66fc610f1fef4b8297ceeb6b39de68b9eebc6
zeenatkhan11 <zeenatkhan14sep@gmail.com>
2024-03-21 20:05:59 +0500

    push my calculator on github

---
+++ app.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "number1",
+        message: "Enter your 1st number: "
+    },
+    {
+       type: "number", 
+       name: "number2",
+       message: "Enter 2nd number:"
+    },
+    {
+        type: "list", 
+        name: "operator",
+        choices: ["+","-","*","/","%","**"],
+        message: "Select Operator:",
+     },
+
+]);
+
+ if(answer.operator ==="+"){
+   console.log(answer.number1 + answer.number2);
+    
+ } else if(answer.operator ==="-"){
+   console.log(answer.number1 - answer.number2);
+    
+ } else if(answer.operator ==="*"){
+   console.log(answer.number1 * answer.number2);
+   
+ }
+ else if(answer.operator ==="/"){
+   console.log(answer.number1 / answer.number2);
+  
+ }
+
+ else if(answer.operator ==="%"){
+  console.log(answer.number1 % answer.number2);
+   
+}
+else if(answer.operator ==="**"){
+  console.log(answer.number1 ** answer.number2);
+   
+}
+ else{
+    console.log("enter valid input");
+ }
+
+ 
+
